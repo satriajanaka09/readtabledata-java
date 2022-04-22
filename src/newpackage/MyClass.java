21b9c15f02ae602ff11bcd32fd860b1103839b05
satriajnk <satriajanaka09@gmail.com>
2022-04-22 22:37:58 +0700

    tidying code

--- src/newpackage/MyClass.java
+++ src/newpackage/MyClass.java
@@ -17,27 +17,27 @@ public class MyClass {
 	    	
 			
 			int Row_count = driver.findElements(By.xpath("//*[@id=\"customers\"]/tbody/tr")).size();
-			 System.out.println("Number Of Rows = "+Row_count);
+			System.out.println("Number Of Rows = "+Row_count);
 
 			
-			 int Col_count = driver.findElements(By.xpath("//*[@id=\"customers\"]/tbody/tr[2]/td")).size();
-			 System.out.println("Number Of Columns = "+Col_count);
+			int Col_count = driver.findElements(By.xpath("//*[@id=\"customers\"]/tbody/tr[2]/td")).size();
+			System.out.println("Number Of Columns = "+Col_count);
 
 			 
-			 String first_part = "//*[@id=\"customers\"]/tbody/tr[";
-			 String second_part = "]/td[";
-			 String third_part = "]";
+			String first_part = "//*[@id=\"customers\"]/tbody/tr[";
+			String second_part = "]/td[";
+			String third_part = "]";
 
-			 //Used for loop for number of rows.
+			 
 			 for (int i=2; i<=Row_count; i++){
 			  
-			  //Used for loop for number of columns.
+			  
 			  for(int j=1; j<=Col_count; j++){
 			   
-			   //Prepared final xpath of specific cell as per values of i and j.
+			   
 			   String final_xpath = first_part+i+second_part+j+third_part;
 			   
-			   //Will retrieve value from located cell and print It.
+			   
 			   String Table_data = driver.findElement(By.xpath(final_xpath)).getText();
 			   System.out.print(Table_data +" ");
 			  }
